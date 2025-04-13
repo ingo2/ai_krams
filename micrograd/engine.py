2e573ad24c55b359ed1330ef314e6ad419e38d3f
ingo2 <7080543+ingo2@users.noreply.github.com>
2025-04-13 15:59:01 +0200

    a few more tests for Value

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -8,6 +8,9 @@ class Value:
         self._prev = set(_children)
         # The op that produced this node, for graphviz / debugging / etc.
         self._op = _op
+      
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
--- micrograd/tests/test_engine.py
+++ micrograd/tests/test_engine.py
@@ -1,6 +1,20 @@
 from engine import Value
 
 
+def test_value():
+    a = Value(2.0)
+    assert a.data == 2.0
+    assert a.grad == 0
+    assert a._prev == set()
+    assert a._op == ""
+    assert repr(a) == "Value(data=2.0, grad=0)"
+
+
+def test_value_repr():
+    a = Value(5.0)
+    assert repr(a) == "Value(data=5.0, grad=0)"
+
+
 def test_add():
     a = Value(2.0)
     b = Value(3.0)
@@ -17,3 +31,25 @@ def test_mul():
     assert c._op == "*"
     assert c.data == 6.0
     assert c._prev == {a, b}
+
+
+def test_add_mul():
+    a = Value(2.0)
+    b = Value(3.0)
+    c = Value(-4.0)
+    d = a + b * c
+    assert d._op == "+"
+    assert d.data == 2.0 + 3.0 * -4.0
+    assert len(d._prev) == 2
+    assert {child.data for child in d._prev} == {2.0, -12.0}
+
+
+def test_mul_add():
+    a = Value(2.0)
+    b = Value(3.0)
+    c = Value(-5.0)
+    d = a * b + c
+    assert d._op == "+"
+    assert d.data == 2.0 * 3.0 + -5.0
+    assert len(d._prev) == 2
+    assert {child.data for child in d._prev} == {-5.0, 6.0}
