25421be11f7e04e6f1210867ae1d9e8e42a5d7f0
ingo2 <7080543+ingo2@users.noreply.github.com>
2025-04-17 09:56:07 +0200

    Update test_neural_net.py

--- micrograd/tests/test_neural_net.py
+++ micrograd/tests/test_neural_net.py
@@ -1,15 +1,41 @@
+import random
+import sys
 from engine import Value
-from neural_net import Neuron
+from neural_net import MLP
 import pytest
-import viz_tools
 
+# pytest -s
+def test_training_loop():
+    random.seed(657)
 
-def test_neuron():
-    # No assertions. Just runs the thing.
-    n = Neuron(2)
-    out = n([1, 2])
-    out.backward()
+    # Possible inputs.
+    xs = [[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0], [1.0, 1.0, -1.0]]
+    # Desired targets.
+    ys = [1.0, -1.0, -1.0, 1.0]
+    # Initialze the MLP.
+    n = MLP(3, [4, 4, 1])
 
-    # Uncommend to optionally draw the graph.
-    # graph = viz_tools.draw_dot(out)
-    # graph.view()
+    # Training loop.
+    loss = Value(sys.float_info.max)
+    loss_prev = loss.data
+    for k in range(10):
+        # Compute predictions and loss (forward pass).
+        ypred = [n(x) for x in xs]
+        loss = sum((yout - ygt) ** 2 for ygt, yout in zip(ys, ypred))
+
+        # Reset and recompute gradients (backward pass).
+        n.zero_grad()
+        loss.backward()
+
+        # Apply gradient decent.
+        for p in n.parameters():
+            p.data += -0.01 * p.grad
+
+        decr = loss_prev - loss.data
+        loss_prev = loss.data
+
+        print(f"{k}: loss = {loss.data}, decr = {decr}")
+        assert decr > 0.0
+
+    # Regression check.
+    loss.data == pytest.approx(0.23246414201773874, rel=1e-6)
