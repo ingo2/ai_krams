134b121bc4b21d203b893f44664f2eafe17612db
ingo2 <7080543+ingo2@users.noreply.github.com>
2025-04-17 09:55:45 +0200

    Update neural_net.py

--- micrograd/neural_net.py
+++ micrograd/neural_net.py
@@ -2,14 +2,72 @@ import random
 from engine import Value
 
 
-class Neuron:
-    def __init__(self, nin):
-        self.weights = [Value(random.uniform(-1, 1), label=f"w{i+1}") for i in range(nin)]
-        self.bias = Value(random.uniform(-1, 1), label="b")
+class Module:
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+
+class Neuron(Module):
+
+    def __init__(self, nin, nonlin=True):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(0)
+        # If a non-linear activation function should be applied (inner layers) or not (output
+        # layer).
+        self.nonlin = nonlin
 
     def __call__(self, x):
-        activation = sum(
-            (wi * Value(xi, label=f"x{i+1}") for i, (wi, xi) in enumerate(zip(self.weights, x))),
-            self.bias,
-        )
-        return activation.tanh()
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+        return act.tanh() if self.nonlin else act
+
+    def parameters(self):
+        return self.w + [self.b]
+
+    def __repr__(self):
+        return f"{'tanh' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+
+
+class Layer(Module):
+    """A layer of neurons."""
+
+    def __init__(self, nin, nout, **kwargs):
+        # kwargs is a python thing to pass stuff in form of a dictionary into class which does
+        # things with it :O. Here the "nonLin" konfiguration is passed into the Neuron.
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+
+class MLP(Module):
+    """Multi-layer perceptron."""
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        # Internal layers should apply the non-linearity, the output layer should not.
+        self.layers = [
+            Layer(sz[i], sz[i + 1], nonlin=i != len(nouts) - 1) for i in range(len(nouts))
+        ]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
