1ee160534f823d63478367fda9cd69ba99d68926
ingo2 <7080543+ingo2@users.noreply.github.com>
2025-04-20 11:15:52 +0200

    Copy example training-loop to main.

--- micrograd/main.py
+++ micrograd/main.py
@@ -1,24 +1,39 @@
+import random
+import sys
 import viz_tools
 from engine import Value
 from matplotlib import pyplot as plt
+from neural_net import MLP
 
 
 def main():
-    h = 0.0001
+    random.seed(657)
 
-    a = Value(2.0, label="a")
-    b = Value(-3.0, label="b")
-    c = Value(10.0, label="c")
-    e = a * b
-    e.label = "e"
-    d = e + c
-    d.label = "d"
-    f = Value(-2.0, label="f")
-    L = d * f
-    L.label = "L"
-    L.backward()
+    # Possible inputs.
+    xs = [[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0], [1.0, 1.0, -1.0]]
+    # Desired targets.
+    ys = [1.0, -1.0, -1.0, 1.0]
+    # Initialze the MLP.
+    n = MLP(3, [4, 4, 1])
 
-    graph = viz_tools.draw_dot(L)
+    loss = Value(sys.float_info.max)
+    loss_prev = loss.data
+    for k in range(10):
+        ypred = [n(x) for x in xs]
+        loss = sum((yout - ygt) ** 2 for ygt, yout in zip(ys, ypred))
+
+        n.zero_grad()
+        loss.backward()
+
+        for p in n.parameters():
+            p.data += -0.01 * p.grad
+
+        decr = loss_prev - loss.data
+        loss_prev = loss.data
+
+        print(f"{k}: loss = {loss.data}, decr = {decr}")
+
+    graph = viz_tools.draw_dot(loss)
     graph.view()
 
 
